44bcbbbdc8047d0e1bcff6ed84bed7d134abb8cf
codernoyon <noyonrahman2003@gmail.com>
2022-08-01 20:47:33 +0600

    new file added

---
+++ reverse_string.py
@@ -0,0 +1,12 @@
+text = "I love you more than I can eat."
+reverse = ""
+for char in text:
+    reverse = char + reverse
+
+print(reverse)
+
+
+# The short way
+# text = "I love you more than I can eat."
+# reverse = text[::-1]
+# print(reverse)
